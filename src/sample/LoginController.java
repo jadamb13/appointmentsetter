41b925a8f131b307d9a7bf3eed506832f68a249f
jbenn88 <jamesa.benn@gmail.com>
2023-04-17 18:49:34 -0400

    Project has been connected to local MySQL database and javadocs comments added for created controllers

--- src/sample/LoginController.java
+++ src/sample/LoginController.java
@@ -7,23 +7,33 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 
-
+/** A Controller class for the LoginView. */
 public class LoginController implements Initializable {
 
     Stage stage;
     Parent scene;
 
+    /**
+     Initializes LoginView.
+
+     @param url location information
+     @param resourceBundle resource information
+     */
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         System.out.println("LoginView Initialized.");
     }
 
+    /**
+     * Displays the MainView after receiving valid login credentials.
+     *
+     * @param actionEvent represents user clicking on "Login" button
+     */
     public void onActionDisplayMainView(ActionEvent actionEvent) throws IOException {
         System.out.println("Clicked Login.");
         stage = (Stage) ((Button) actionEvent.getSource()).getScene().getWindow();
--- src/sample/MainViewController.java
+++ src/sample/MainViewController.java
@@ -1,10 +1,23 @@
 package sample;
 
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+/** A Controller class for the MainView. */
 public class MainViewController implements Initializable {
+
+    Stage stage;
+    Parent scene;
+
+    /**
+     Initializes MainView.
+
+     @param url location information
+     @param resourceBundle resource information
+     */
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         System.out.println("MainView Initialized.");
